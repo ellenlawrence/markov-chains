0aa0d6ac71766684c8aa29e8b3d3c547b9c55e15
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-16 17:57:30 -0700

    Edited to ensure capitalization

--- markov.py
+++ markov.py
@@ -44,13 +44,9 @@ def make_chains(text_string):
     
     words = text_string.split()
 
-    for index, word in enumerate(words):
-        if index == len(words) - 2:
-            break
-        chains[(word, words[index + 1])].append(words[index + 2])
+    for index in range(len(words) - 2):
+        chains[(words[index], words[index + 1])].append(words[index + 2])
 
-    # for chain, words in chains.items():
-    #     print(f'{chain}: {words}')
 
     return chains
 
@@ -60,16 +56,25 @@ def make_text(chains):
 
     words = []
 
-    keys_chain = chains.keys() #makes a list of the keys in chains
-    keys_chain = list(keys_chain) #made keys_chain into an iterable list
+    # keys_chain = chains.keys() #makes a list of the keys in chains
+    # keys_chain = list(keys_chain) #made keys_chain into an iterable list
+    keys_chain = list(chains)
     bigrm = choice(keys_chain) #randomly chooses a tuple from keys
+    print(bigrm)
+    if bigrm[0][0] == bigrm[0][0].upper():
+        words.extend(bigrm)
+    elif bigrm[1][0] == bigrm[1][0].upper():
+        words.extend(bigrm[1])
     
-    words.append(bigrm[0]) 
-    words.append(bigrm[1])
+    # words.append(bigrm[0]) 
+    # words.append(bigrm[1])
 
-    while len(words) < 50 and bigrm in chains:
+    while len(words) < 100 and bigrm in chains:
         new_word = choice(chains[bigrm])
-        words.append(new_word)
+        print(new_word)
+        if bigrm[1] in words or new_word[0] == new_word[0].upper():
+            words.append(new_word)
+
         bigrm = (bigrm[1], new_word)
 
 
